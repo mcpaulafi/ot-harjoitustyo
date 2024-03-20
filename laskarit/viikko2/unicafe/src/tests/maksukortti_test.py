302a649abf554271d6df7435cacc787712348a04
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-03-20 17:27:18 +0200

    All tests for maksukortti

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,20 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+        
+    def test_kortin_saldo_on_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.maksukortti.lataa_rahaa(2500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 35.0)
+
+    def test_kortilta_voi_ottaa_rahaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+
+    def test_kortilta_ei_voi_ottaa_rahaa_yli_saldon(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1001), False)
+
+    def test_kortin_saldon_tulostus_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
