af21ea8ffe29c8bbb5e868804023c4a383e24961
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-04-04 19:10:33 +0300

    Index: creating of Tkinter window

--- src/index.py
+++ src/index.py
@@ -1,7 +1,18 @@
+from tkinter import Tk, ttk
+from ui.ui import UI
 
 
-print("INDEX")
+def main():
+    window = Tk()
+    window.geometry("640x480")
+    window.title("My Weather Window")
 
+    ui_view = UI(window)
+    ui_view.start()
+
+    window.mainloop()
+
+print("MAIN\n")
 
 if __name__ == "__main__":
-    pass
+    main()
