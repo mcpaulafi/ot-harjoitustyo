7b70ad3f048a617455725acebc4c059318bc44f6
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-04-04 19:13:14 +0300

    Station repo: renaming file, find all and return by row

---
+++ src/repositories/station_repository.py
@@ -0,0 +1,49 @@
+from entities.station import Station #TODO: HOW THIS WORKS?
+from database_connection import get_database_connection
+
+def get_station_by_row(row):
+    #print("ROW", row["station_id"], row["name"])
+    return Station(station_id=row["station_id"], fmisid=row["fmisid"], name=row["name"], 
+                   nickname=row["nickname"], lat=row["lat"], lon=row["lon"]) if row else None
+
+class StationRepository:
+    """Class for Weather Station list operations.
+    """
+    def __init__(self, connection):
+        """Class constructor.
+
+        Args:
+            connection: Database connection object
+        """
+        self._connection = connection
+
+
+    def find_all(self):
+        """Returns all stations.
+
+        Returns:
+            Station objects.
+        """
+
+        cursor = self._connection.cursor()
+
+        cursor.execute("select * from stations ORDER BY name ASC")
+
+        rows = cursor.fetchall()
+
+
+        return list(map(get_station_by_row, rows))
+
+# TODO: Find by name? Find by id? Create new?
+
+    def delete_all(self):
+        """Removes all stations.
+        """
+
+        cursor = self._connection.cursor()
+
+        cursor.execute("delete from stations")
+
+        self._connection.commit()
+
+station_repository = StationRepository(get_database_connection())
