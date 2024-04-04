43f4d9357e2e9eade904092dda302125e777ce91
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-04-04 19:17:04 +0300

    Station service: returns all stations as list of objects

---
+++ src/services/station_service.py
@@ -0,0 +1,29 @@
+from entities.station import Station
+
+from repositories.station_repository import (
+    station_repository as default_station_repository
+)
+
+class StationService:
+    def __init__(
+        self,
+        station_repository=default_station_repository
+    ):
+        """Class constructor. Creates new service responsible for app logic.
+
+        Args:
+            station_repository:
+                Optional, default StationRepository object.
+                Object has StationRepository class methods.
+        """
+        self._station_repository = station_repository
+
+    def get_stations(self):
+        """Returns all stations.
+
+        Returns:
+            List of Station objects.
+        """
+        return self._station_repository.find_all()
+    
+station_service = StationService()
