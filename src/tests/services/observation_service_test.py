986b2d2cf7edde437584b2d6d7b2e6e3970be2c2
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-04-28 21:33:17 +0300

    Totally renewed testing

--- src/tests/services/observation_service_test.py
+++ src/tests/services/observation_service_test.py
@@ -1,152 +1,70 @@
 import unittest
-from datetime import timezone
 import datetime as dt
-import pytz
-from fmiopendata.wfs import download_stored_query
-from services.station_service import station_service
 from entities.observation import Observation
-from database_connection import get_database_connection
 
 
 def get_observation_by_row(row):
     return Observation(station_id=row["station_id"], temperature=row["temperature"],
-                   wind=row["wind"], wind_direction=row["wind_direction"],
-                   datetime=row["datetime"]) if row else None
+                       wind=row["wind"], wind_direction=row["wind_direction"],
+                       datetime=row["datetime"], error_msg=["error_msg"]) if row else None
+
+# Copied 28.4.2024 from real
 
 class FakeObservationRepository:
-    def __init__(self, connection=get_database_connection()):
+    def __init__(self, connection=1):
         self._connection = connection
 
     def get_data_from_fmi(self, station_id):
-        station_lat = 0
-        station_lon = 0
-        station_name = ""
-        for s in station_service.get_name(station_id):
-            station_lat = s.lat
-            station_lon = s.lon
-            station_name = s.name
-        extra = 0.2
-        #ERROR Inari Seitalaassa, muitakin
-
-
-        # At FMI data is stored in a coordinate squares = bounding boxes.
-        # Formula for an area in which the station is located.
-        # Entire Finland bbox=18.7,59.6,31.4,70.2"
-
-        station_box = f"{str(float(station_lon)-extra)},{str(float(station_lat)-extra)},\
-        {str(float(station_lon)+extra)},{str(float(station_lat)+extra)}"
-
-        # Retrieve the latest hour of data
-        end_time = dt.datetime.now(timezone.utc)
-        start_time = end_time - dt.timedelta(hours=1)
-
-        # Convert times to properly formatted strings
-        start_time = start_time.isoformat(timespec="seconds") + "Z"
-        end_time = end_time.isoformat(timespec="seconds") + "Z"
-        # -> 2020-07-07T13:00:00Z
-
-        # Retrievind data for the box
-        obs2 = download_stored_query("fmi::observations::weather::multipointcoverage",
-                                    args=[f"bbox={station_box}",
-                                        "timeseries=True"])
-        # All data
-        # print(sorted(obs2.data.keys()))
-
-        # The times are as a list of datetime objects, printing latest obs
-        utc_datetime = str(obs2.data[station_name]['times'][-1])
-
-        utc_format = "%Y-%m-%d %H:%M:%S"
-        local_tz = pytz.timezone('Europe/Helsinki')
-
-        utc_dt = dt.datetime.strptime(utc_datetime, utc_format)
-        local_dt = str(utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz))[:-6]
-
-        #print("UTC", utc_datetime, "Local", local_dt)
-
-        temperature = obs2.data[station_name]['Air temperature']['values'][-1]
-        #print("Temperature:", temperature)
-
-        wind = obs2.data[station_name]['Wind speed']['values'][-1]
-        wind_direction = obs2.data[station_name]['Wind direction']['values'][-1]
-        #print("Wind:", wind, "m/s direction:", wind_direction)
-
-        return (temperature, wind, wind_direction, local_dt)
+        if station_id == 1:
+            get_error = 0
+            local_dt = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            temperature = 12
+            wind = 4
+            wind_direction = 200
+            return (temperature, wind, wind_direction, local_dt, get_error)
+        return False
 
     def save_observation(self, station_id):
-        cursor = self._connection.cursor()
-        obs = self.get_data_from_fmi(station_id)
-
-        temperature = obs[0]
-        wind = obs[1]
-        wind_direction = obs[2]
-        date_str = obs[3]
-
-        cursor.execute(
-            '''insert into observations (station_id, temperature, wind, wind_direction, datetime) 
-                    values (?, ?, ?, ?, ?)''',
-                    (str(station_id), temperature, wind, wind_direction, str(date_str))
-                )
-
-        self._connection.commit()
-
+        if station_id == 1:
+            return True
+        return False
 
     def find_observation(self, station_id):
-        """Returns latest observations for a station.
-
-            Returns:
-                Observation objects.
-            """
-
-        cursor = self._connection.cursor()
-
-        cursor.execute("select * from observations where station_id=? \
-                       ORDER BY observation_id DESC LIMIT 1", (str(station_id),))
-
-        rows = cursor.fetchall()
-
-        return list(map(get_observation_by_row, rows))
+        if station_id==1:
+            # Fake output from database
+            test_row = []
+            test_row1 = {}
+            test_row1["station_id"] = "1"
+            test_row1["temperature"] = "10"
+            test_row1["wind"] = "5"
+            test_row1["wind_direction"] = "150"
+            test_row1["datetime"] = "2024-04-28 12:00:01"
+            test_row1["error_msg"] = 0
+            test_row.append(test_row1)
+            return get_observation_by_row(test_row1)
+        return False
 
     def delete_all(self):
-        """Removes all observations from observations table.
-        """
+        return True
 
-        cursor = self._connection.cursor()
-
-        cursor.execute("delete from observations")
-
-        self._connection.commit()
-
-
+# Testing begins
 
 class TestObservationService(unittest.TestCase):
     def setUp(self):
-        self._observation_repository=FakeObservationRepository()
+        #observation_service.delete_observations_from_database()
+        self._observation_repository = FakeObservationRepository()
+        self.wind = 0
+        self.station_id = 1
+
+    def test_update_observation(self):
+        test_save = self._observation_repository.save_observation(self.station_id)
+        return self.assertEqual(test_save, True)
 
     def test_get_observation(self):
-        # Save observation first
-        station_id = 1
-        self._observation_repository.save_observation(station_id)
-        
-        get_obs = self._observation_repository.find_observation(station_id)
-        test_id = 20
-        for n in get_obs:
-            if n.station_id == station_id:
-                test_id = n.station_id
+        test_result = self._observation_repository.find_observation(self.station_id)
+        #Why ERROR here?
+        wind_result  = int(test_result.wind)
+        return self.assertEqual(wind_result, 5)
 
-        return self.assertEqual(test_id, 1)
-    
-    def test_update_observation(self):
-        # Save observation first
-        station_id = 1
-        self._observation_repository.save_observation(station_id)
-
-        get_name = self._observation_repository.find_observation(station_id)
-        test_id = 20
-        for n in get_name:
-            if n.station_id == station_id:
-                test_id = n.station_id
-
-        return self.assertEqual(test_id, 1)
-
-#    def test_delete_observations_from_database(self):
-#        self._observation_repository.delete_all()
+    def test_delete_observations_from_database(self):
+        return self.assertEqual(self._observation_repository.delete_all(), True)
