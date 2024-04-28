74aeec8c2ccc667c608143b65a23b6798b7ed6b0
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-04-28 22:14:47 +0300

    Link issues fixed

--- src/tests/services/station_service_test.py
+++ src/tests/services/station_service_test.py
@@ -1,4 +1,5 @@
 import unittest
+from services.station_service import StationService
 from entities.station import Station
 
 def get_station_by_row(row):
@@ -6,16 +7,7 @@ def get_station_by_row(row):
                    name=row["name"], lat=row["lat"],
                    lon=row["lon"], source=row["source"]) if row else None
 
-# Fake output from database
-test_row = []
-test_row1 = {}
-test_row1["station_id"] = "1"
-test_row1["original_id"] = "100"
-test_row1["name"] = "test_name"
-test_row1["lat"] = "10"
-test_row1["lon"] = "20"
-test_row1["source"] = "testing"
-test_row.append(test_row1)
+
 
 # Copied 28.4.2024 from real
 
@@ -25,13 +17,22 @@ class FakeStationRepository:
 
     def __init__(self, connection=1):
         self._connection = connection
+        # Fake output from database
+        self.test_row = []
+        self.test_row1 = {}
+        self.test_row1["station_id"] = "3"
+        self.test_row1["original_id"] = "100"
+        self.test_row1["name"] = "test_name"
+        self.test_row1["lat"] = "10"
+        self.test_row1["lon"] = "20"
+        self.test_row1["source"] = "testing"
+        self.test_row.append(self.test_row1)
 
     def count_all(self):
         return 3
 
     def find_all(self):
-        rows = ""
-        return list(map(get_station_by_row, rows))
+        return list(map(get_station_by_row, self.test_row))
 
     def count_selected_stations(self):
         return 4
@@ -50,16 +51,16 @@ class FakeStationRepository:
         return False
 
     def find_selected(self):
-        return list(map(get_station_by_row, test_row))
+        return list(map(get_station_by_row, self.test_row))
 
     def find_station(self, station_id):
         if station_id == 1:
-            return list(map(get_station_by_row, test_row))
+            return list(map(get_station_by_row, self.test_row))
         return False
 
     def find_nickname(self, station_id):
         if station_id == 1:
-            station = list(map(get_station_by_row, test_row))
+            station = list(map(get_station_by_row, self.test_row))
             station[0].set_nickname("testnick")
             return station
         return False
@@ -67,7 +68,7 @@ class FakeStationRepository:
     def find_error(self, station_id):
         if station_id == 1:
             error_msg = 1
-            station = list(map(get_station_by_row, test_row))
+            station = list(map(get_station_by_row, self.test_row))
             station[0].set_error_msg(error_msg)
             return station
         return False
@@ -80,34 +81,39 @@ class FakeStationRepository:
 
 class TestStationService(unittest.TestCase):
     def setUp(self):
+        self._station_service = StationService(FakeStationRepository())
         self._station_repository = FakeStationRepository()
+        self.station_id = 4
 
-    def get_stations(self):
-        return self.assertEqual(self._station_repository.find_all(), 3)
+    def test_get_stations(self):
+        test_result = self._station_service.get_stations()
+        return self.assertEqual(len(test_result), 1)
 
     def test_get_station(self, station_id=1):
-        return self.assertEqual(self._station_repository.find_station(station_id)[0].name, "test_name")
+        test_result = self._station_service.get_station(station_id)
+        return self.assertEqual(test_result[0].name, "test_name")
 
     # Selected stations
     def test_count_selected(self):
-        return self.assertEqual(self._station_repository.count_selected_stations(), 4)
+        test_result = self._station_service.count_selected()
+        return self.assertEqual(test_result, 4)
 
     def test_save_selected(self, station_id=4):
         self._station_repository.save_selected_station_to_database(station_id)
-        return self.assertEqual(self._station_repository.save_selected_station_to_database(station_id), True)
+        return self.assertEqual(self._station_service.save_selected(station_id), True)
 
-    def test_save_selected_nickname(self, station_id=5, nickname="Testing123"):
-        return self.assertEqual(self._station_repository.save_nickname_to_database(
+    def test_save_selected_nickname(self, station_id=4, nickname="Testing123"):
+        return self.assertEqual(self._station_service.save_selected_nickname(
             station_id, nickname), True)
 
     def test_get_selected(self):
-        return self.assertEqual(len(self._station_repository.find_selected()), 1)
+        return self.assertEqual(len(self._station_service.get_selected()), 1)
 
     def test_get_nickname(self, station_id=1):
-        return self.assertEqual(self._station_repository.find_nickname(station_id)[0].nickname, "testnick")
+        return self.assertEqual(self._station_service.get_nickname(station_id)[0].nickname, "testnick")
 
     def test_get_error(self, station_id=1):
-        return self.assertEqual(self._station_repository.find_error(station_id)[0].error_msg, 1)
+        return self.assertEqual(self._station_service.get_error(station_id)[0].error_msg, 1)
 
     def test_delete_selected(self):
-        return self.assertEqual(self._station_repository.delete_selected_stations_from_database(), True)
+        return self.assertEqual(self._station_service.delete_selected(), True)
