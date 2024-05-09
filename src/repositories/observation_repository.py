a683fc7f140ec6a5ac0e8dfaccabe941d515929f
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-05-09 22:40:50 +0300

    Spliting long FMI function and added function for same date check. Code cleanup.

--- src/repositories/observation_repository.py
+++ src/repositories/observation_repository.py
@@ -8,41 +8,46 @@ from database_connection import get_database_connection
 
 
 def get_observation_by_row(row):
-    """Function converts database result and returns it as Observation object.
+    """Function which converts database result and returns it as an Observation object.
+
     Args: 
-        row: contains a library
+        row: contains a library for object attributes
     Returns:
         Observation object"""
+    
     return Observation(station_id=row["station_id"], temperature=row["temperature"],
                        wind=row["wind"], wind_direction=row["wind_direction"],
                        datetime=row["datetime"], error_msg=row["error_msg"]) if row else None
 
 
 class ObservationRepository:
-    """Class for Observation operations.
+    """Class for operations of Observations.
+
+    Attributes:
+        connection: object which contains database connection
     """
 
     def __init__(self, connection):
-        """Class constructor.
+        """Class constructor which creates a new observation.
+
         Args:
-            connection: Database connection object
+            connection: mandatory, database connection object
         """
         self._connection = connection
 
     def get_data_from_fmi(self, station_id):
-        """ Gets data for the station from FMI interphase. 
+        """ Method which gets observation data for the station from FMI interphase.
         At FMI data is stored in a coordinate (lat,lon) squares = bounding boxes.
-        Stations provide new data every 1-10 minutes. 
-        Timezone is UTC which is converted in return to local time.
+        Stations provide new data every 1-10 minutes and
+        query returns all data from latest hour.
+        Calls for function to check data.
+
         Request to FMI includes: 
-            bounding box around the coordinates of a station.
-        Reply is filtered:
-            leatest timestamp (contains latest hour)
-            station name
+            bounding box around the coordinates of the station.
+
         Args:
-            station_id
-        Returns:
-            tuple (temperature, wind, wind_direction, date, error_message)
+            station_id: mandatory, to get parameters from the database for the query
+
         """
         if not station_service.get_station(station_id):
             return False
@@ -50,7 +55,8 @@ class ObservationRepository:
         station_lat = 0
         station_lon = 0
         station_name = ""
-        for s in station_service.get_station(station_id):
+        s = station_service.get_station(station_id)
+        if s:
             station_lat = s.lat
             station_lon = s.lon
             station_name = s.name
@@ -62,6 +68,24 @@ class ObservationRepository:
         obs2 = download_stored_query("fmi::observations::weather::multipointcoverage",
                               args=[f"bbox={station_box}","timeseries=True"])
 
+        return self.check_data_from_fmi(obs2, station_name)
+
+    def check_data_from_fmi(self, obs2, station_name):
+        """Function which checks data received from FMI.
+        If no station data is retrieved, sets all as None and error_msg as 1.
+        Timezone is UTC which is converted in return to local time.
+
+        Args:
+            obs2: result of query from FMI
+            station_name as string
+
+        Reply is filtered with:
+            latest timestamp
+            station name
+
+        Returns:
+            tuple (temperature, wind, wind_direction, date, error_message)
+        """
         get_error = 0
         try:
             utc_datetime = str(obs2.data[station_name]['times'][-1])
@@ -72,11 +96,11 @@ class ObservationRepository:
             local_dt = str(utc_dt.replace(
                 tzinfo=pytz.utc).astimezone(local_tz))[:-6]
         except KeyError:
-            print("Following station not found. No data retrieved.")
             utc_datetime = None
             local_dt = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             get_error = 1
 
+        # TODO check data
         if utc_datetime is not None:
             temperature = obs2.data[station_name]['Air temperature']['values'][-1]
             wind = obs2.data[station_name]['Wind speed']['values'][-1]
@@ -104,12 +128,33 @@ class ObservationRepository:
             return True
         return False
 
+    def check_if_same_date(self, first_date, second_date):
+        """Function checks if dates are same.
+        Args:
+            first_date as string
+            second_date as string
+        Returns:
+            True if dates are same
+            False if not
+        """
+        datetime_object1 = first_date
+        datetime_object2 = second_date
+
+        if datetime_object1 == datetime_object2:
+            return True
+        return False
+
     def save_observation(self, station_id):
-        """ Saves observation to the database. Checks if 
-        in the database there is already new data.
+        """Function retrieves new data from FMI and
+        saves observation to the database.
+        Checks if station_id is valid in the database.
+        Does nothing if retrieved date already is in the database.
+
         Args:
             station_id: id of the selected station
-            """
+        Returns:
+            True"""
+
         if not station_service.get_station(station_id):
             return False
 
@@ -128,17 +173,19 @@ class ObservationRepository:
         else:
             error_msg = 0
 
+        db_obs = self.find_observation(station_id)
 
-        cursor.execute(
-            '''insert into observations (station_id, temperature, \
-                wind, wind_direction, datetime, error_msg) 
-                    values (?, ?, ?, ?, ?, ?)''',
-            (str(station_id), temperature, wind,
-            wind_direction, str(date_str), error_msg)
-        )
+        if not db_obs or not self.check_if_same_date(date_str, db_obs.datetime):
 
-        self._connection.commit()
+            cursor.execute(
+                '''insert into observations (station_id, temperature, \
+                    wind, wind_direction, datetime, error_msg) 
+                        values (?, ?, ?, ?, ?, ?)''',
+                (str(station_id), temperature, wind,
+                wind_direction, str(date_str), error_msg)
+            )
 
+            self._connection.commit()
         return True
 
 
@@ -166,7 +213,8 @@ class ObservationRepository:
             return False
 
     def delete_all(self):
-        """Removes all observations from observations table.
+        """Function removes all observations from observations table.
+
         Returns: 
             True if no errors.
                 """
@@ -180,4 +228,3 @@ class ObservationRepository:
 
 
 observation_repository = ObservationRepository(get_database_connection())
-
