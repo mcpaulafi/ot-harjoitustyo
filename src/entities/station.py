22f13dfdaf4e81855815e1ea3cce0c8f6cf42cb0
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-05-09 20:56:47 +0300

    Code clean up

--- src/entities/station.py
+++ src/entities/station.py
@@ -1,13 +1,15 @@
 class Station:
-    """Class for a weather station
+    """Class for a weather station.
 
     Attributes:
         station_id: String for station id in this app.
-        fmisid: String for station id by source.
+        original_id: String for id used by the source.
         name: String for station name by source.
         nickname: String for station name by user.
         lat: String for latitude of the station location.
         lon: String for longtitude of the station location.
+        source: String for where the station originally comes from.
+        error_msg: String for error message.
     """
 
     def __init__(self, station_id=None, original_id=None, name=None,
@@ -15,7 +17,13 @@ class Station:
         """Class constructor, creates a new station.
 
         Args:
-            Same as class attributes.
+            station_id: String for station id in this app.
+            original_id: String for id used by the source.
+            name: String for station name by source.
+            nickname: String for station name by user.
+            lat: String for latitude of the station location.
+            lon: String for longtitude of the station location.
+            source: String for where the station originally comes from.
         """
 
         self.station_id = station_id
@@ -29,22 +37,34 @@ class Station:
 
     def set_nickname(self, nickname):
         """Method to set nickname.
+
         Args: 
-        nickname"""
+            nickname string
+        Returns:
+            True"""
         self.nickname = nickname
         return True
 
     def get_nickname(self):
-        """Method to get nickname."""
+        """Method to get nickname.
+
+        Returns:
+            nickname as string"""
         return self.nickname
 
     def set_error_msg(self, error_msg):
-        """Method to set nickname.
+        """Method to set error message.
+
         Args: 
-        nickname"""
+            error_msg as string
+        Returns:
+            True"""
         self.error_msg = error_msg
         return True
 
     def get_error_msg(self):
-        """Method to get nickname."""
+        """Method to get error message.
+
+        Returns:
+            error message as a string"""
         return self.error_msg
