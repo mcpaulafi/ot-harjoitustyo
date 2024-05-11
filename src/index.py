3fdeac0932895185f1fd490f4f5d87133febaa5c
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-05-12 01:20:56 +0300

    Background color

--- src/index.py
+++ src/index.py
@@ -2,19 +2,17 @@ from tkinter import Tk
 from ui.ui import UI
 
 
+
 def main():
     window = Tk()
     window.wm_geometry("640x480")
+    window.configure(bg='white')
     window.title("My Weather Window")
-    window.configure(background="LightBlue1")
 
     ui_view = UI(window)
     ui_view.start()
 
     window.mainloop()
-
-
-print("MAIN\n")
 
 if __name__ == "__main__":
     main()
